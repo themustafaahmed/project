2907e118410966040d24a1bd3c366e3d0ac53796
themustafaahmed <themustafaahmed@abv.bg>
2017-06-20 19:44:51 +0300

    Add files via upload

---
+++ MapSearchNew/src/graph/MyData.java
@@ -0,0 +1,35 @@
+package graph;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class MyData implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private ArrayList<MyPoint> arrMyPoint;
+	private ArrayList<MyLine> arrMyLine;
+
+	final int r = 15, r2 = 2 * r;
+
+	public ArrayList<MyPoint> getArrMyPoint() {
+		return arrMyPoint;
+	}
+
+	public void setArrMyPoint(ArrayList<MyPoint> arrMyPoint) {
+		this.arrMyPoint = arrMyPoint;
+	}
+
+	public ArrayList<MyLine> getArrMyLine() {
+		return arrMyLine;
+	}
+
+	public void setArrMyLine(ArrayList<MyLine> arrMyLine) {
+		this.arrMyLine = arrMyLine;
+	}
+
+	public MyData() {
+		arrMyPoint = new ArrayList<MyPoint>();
+		arrMyLine = new ArrayList<MyLine>();
+	}
+}
