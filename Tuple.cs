62e7d0cb76fd371e75155dcc5515b8d780eda34a
themustafaahmed <themustafaahmed@abv.bg>
2018-02-07 20:23:16 +0200

    Create Tuple.cs

---
+++ Tuple.cs
@@ -0,0 +1,8 @@
+ public static Tuple<long, long, long> nesto()
+                 {
+                 long Id1 = 1;
+                 long Id2 = 2;
+                 long Id3 = 3;
+
+                 return Tuple.Create(Id1, Id2, Id3);
+                 }
